fae2e843bd972090dc81166f8513984033095908
Adam Nihill <adamnihill@gmail.com>
2021-01-07 22:19:07 +0000

    some more work done on flask web server

--- server.py
+++ server.py
@@ -1,10 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, url_for, request, json
 # https://pypi.org/project/silence-tensorflow/
 import silence_tensorflow.auto
 import tensorflow as tf
 import tensorflow.keras as kr 
 import h5py
 import numpy as np
+import flask as fl
+import pandas as pd
 
 model = kr.models.load_model("model.h5")
 
@@ -12,19 +14,23 @@ def predict_power(speed):
     # speed requires upper and lower limit based on dataset 
     if speed > 0 and speed < 25:
         windspeed = np.array([speed])
-        return model.predict(windspeed)
+        prediction =  model.predict(windspeed)
+        return prediction
     else: 
         return 0 
 
-test = predict_power(100)
-
-print(test)
 
 app = Flask(__name__, static_url_path='', static_folder='static')
 
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/power')
+def power():
+    wind = float(fl.request.args.get("wind", 0.0))
+    p = predict_power(wind)
+    return {"power output": p}
 
 
 if __name__ == "__main__":
