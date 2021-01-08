7c7175ae866cc74464ead4a9c2372f2f493d00bc
Adam Nihill <adamnihill@gmail.com>
2021-01-08 15:20:43 +0000

    completed webserver

--- server.py
+++ server.py
@@ -11,13 +11,14 @@ import pandas as pd
 model = kr.models.load_model("model.h5")
 
 def predict_power(speed):
+    speed = float(speed)
     # speed requires upper and lower limit based on dataset 
-    if speed > 0 and speed < 25:
+    if speed >= 1 and speed <= 25:
         windspeed = np.array([speed])
         prediction =  model.predict(windspeed)
         return prediction
     else: 
-        return 0 
+        return "Invalid Input. Please Enter a Number Between 1 and 25."
 
 
 app = Flask(__name__, static_url_path='', static_folder='static')
@@ -26,11 +27,18 @@ app = Flask(__name__, static_url_path='', static_folder='static')
 def index():
     return render_template('index.html')
 
-@app.route('/power')
+@app.route('/power', methods=['GET', 'POST'])
 def power():
-    wind = float(fl.request.args.get("wind", 0.0))
-    p = predict_power(wind)
-    return {"power output": p}
+     if request.method == 'POST' and 'speed' in request.form:
+            speed = request.form['speed']
+            prediction = predict_power(speed)
+            if isinstance(prediction, str):
+                error = prediction
+                return render_template('index.html', msg=error)
+            else:
+                p = prediction[0][0]
+                msg = "The Power Output for Windspeed of %s km/h is equal to %.3f kw." %(speed, p)
+                return render_template('index.html', msg=msg)
 
 
 if __name__ == "__main__":
